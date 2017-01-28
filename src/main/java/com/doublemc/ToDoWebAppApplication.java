7944fa6cbe2ec1e3ca85c39311aebd4567184841
doublemc <ciupa.michal@gmail.com>
2017-01-28 16:09:42 +0100

    Initial commit

---
+++ src/main/java/com/doublemc/ToDoWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.doublemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoWebAppApplication.class, args);
+	}
+}
